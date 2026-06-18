ca757d802e0c34b2da9fada0b2f5f98743cde121
vologzhanin.a <elephantmaker@mail.ru>
2026-06-18 05:05:06 +0400

    IndexSelfCheckTest red

---
+++ tests/TestCase/Controller/Project/IndexSelfCheckTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\TestCase\Controller\Project;
+
+use App\Tests\Infrastructure\ApiTestCase;
+
+/**
+ * @see IndexController
+ */
+final class IndexSelfCheckTest extends ApiTestCase
+{
+    public function test(): void
+    {
+        $this
+            ->request('POST', '/api/project/index', body: <<<JSON
+                {
+                  "path": "/app"
+                }
+                JSON
+            )
+            ->expectedCode(200);
+    }
+}
