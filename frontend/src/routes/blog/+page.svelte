e5a2a485b78d6cae69f318509dbc18b02fb6570a
vologzhanin.a <elephantmaker@mail.ru>
2026-06-22 03:24:39 +0400

    Svelte routing with slug

--- frontend/src/routes/blog/+page.svelte
+++ frontend/src/routes/blog/+page.svelte
@@ -1,7 +1,11 @@
+<script lang="ts">
+    let { data } = $props();
+</script>
+
 <h1>blog</h1>
 
 <ul>
-    <li><a href="/blog/one">one</a></li>
-    <li><a href="/blog/two">two</a></li>
-    <li><a href="/blog/three">three</a></li>
+    {#each data.summaries as {slug,title}}
+        <li><a href="/blog/{slug}">{title}</a></li>
+    {/each}
 </ul>
--- frontend/src/routes/blog/[slug]/+page.svelte
+++ frontend/src/routes/blog/[slug]/+page.svelte
@@ -1,1 +1,6 @@
-<h1>blog post</h1>
+<script lang="ts">
+    let { data } = $props();
+</script>
+
+<h1>{data.post.title}</h1>
+<div>{@html data.post.content}</div>
