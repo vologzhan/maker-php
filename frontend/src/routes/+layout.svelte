f33df9199024e0775daad52f207edf417d9dd6cc
vologzhanin.a <elephantmaker@mail.ru>
2026-06-22 03:05:19 +0400

    Svelte routing

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+	<a href="/blog">blog</a>
+</nav>
 
 {@render children()}
