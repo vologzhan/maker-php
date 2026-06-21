6e05ec8cabe55f201cb7fcee9b63bd2cadcad237
vologzhanin.a <elephantmaker@mail.ru>
2026-06-21 22:00:48 +0400

    Svelte: 16 keyed each blocks

---
+++ frontend/src/Thing.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    const emojis = {
+        apple: '🍎',
+        banana: '🍌',
+        carrot: '🥕',
+        doughnut: '🍩',
+        egg: '🥚'
+    };
+
+    // `name` is updated whenever the prop value changes...
+    let { name } = $props();
+
+    // ...but `emoji` is fixed upon initialisation
+    const emoji = emojis[name];
+</script>
+
+<p>{emoji} = {name}</p>
