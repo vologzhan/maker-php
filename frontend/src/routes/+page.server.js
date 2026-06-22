83a6cc9c5650fbf222efc528e75f739772516a95
vologzhanin.a <elephantmaker@mail.ru>
2026-06-22 12:18:48 +0400

    Svelte form validation

--- frontend/src/routes/+page.server.js
+++ frontend/src/routes/+page.server.js
@@ -1,3 +1,4 @@
+import { fail } from '@sveltejs/kit';
 import * as db from '$lib/server/database.js';
 
 export function load({ cookies }) {
@@ -16,7 +17,15 @@ export function load({ cookies }) {
 export const actions = {
     create: async ({cookies, request}) => {
         const data = await request.formData();
-        db.createTodo(cookies.get('userid'), data.get('description'));
+
+        try {
+            db.createTodo(cookies.get('userid'), data.get('description'));
+        } catch (error) {
+            return fail(422, {
+                description: data.get('description'),
+                error: error.message,
+            })
+        }
     },
 
     delete: async ({cookies, request}) => {
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
-    let {data} = $props();
+    let {data, form} = $props();
 </script>
 
 <div class="centered">
     <h1>todos</h1>
 
+    {#if form?.error}
+        <p class="error">{form.error}</p>
+    {/if}
+
     <form method="POST" action="?/create">
         <label>
             add a todo:
             <input
-                    name="description"
-                    autocomplete="off"
+                name="description"
+                value={form?.description ?? ''}
+                autocomplete="off"
+                required
             />
         </label>
     </form>
