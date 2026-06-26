3eaddd80802c06b4ae2ada1f4414b2c1ea49176b
vologzhanin.a <elephantmaker@mail.ru>
2026-06-26 19:44:22 +0400

    Frontend: directory context menu Set type Controller added

--- frontend/src/routes/filesystem/Directory.svelte
+++ frontend/src/routes/filesystem/Directory.svelte
@@ -4,6 +4,7 @@
     import {slide} from 'svelte/transition';
     import type {DirectoryItemResponse} from "$lib/Response/Project/Filesystem/DirectoryItemResponse";
     import {contextMenu} from '$lib/Store/ContextMenu';
+    import {UpdateDirectoryType} from "$lib/Controller/Project/Filesystem/UpdateDirectoryTypeController";
 
     let {
         dir,
@@ -50,10 +51,12 @@
                     }
                 },
                 {
-                    label: 'Mark as Controller',
-                    action: () => {
-                        console.log('Mark as Controller', dir.name);
-                    }
+                    label: 'Set type Controller',
+                    action: () =>
+                        UpdateDirectoryType(dir.id, {type: 'controller'})
+                            .catch((err: any) => {
+                                console.error(err);
+                            })
                 }
             ]
         });
